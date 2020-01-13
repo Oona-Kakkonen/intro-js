e5b2380a6683717b256dde35af6013ee385a980d
Oona-Kakkonen <59806531+Oona-Kakkonen@users.noreply.github.com>
2020-01-13 13:20:06 +0200

    added some test javascript

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+console.log('That was it');
+
+var a = 1;
+var b = 2;
+var c = a + b;
+
+console.log(c);
+
+if (a > b) {
+    /* 
+    do the following things if a is lesser than b 
+    (multiple lines comment)
+    */
+    console.log('a is lesser than b');
+    console.log(a + ' is lesser than ' + b);
+} 
+else {
+    // do the following things otherwise (one line comment)
+    console.log('a is greater than b');
+}
+
+if (a === 1) {
+    console.log('a is exactly 1');
+}
