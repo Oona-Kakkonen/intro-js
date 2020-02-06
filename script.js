0cf43a4275f52bdd48d4b7bb173e9b663e0eb8db
Oona-Kakkonen <59806531+Oona-Kakkonen@users.noreply.github.com>
2020-02-06 11:40:33 +0200

    tasks

--- script.js
+++ script.js
@@ -12,10 +12,31 @@ jQuery(document).ready(function() {
     $("html, body").animate({ scrollTop: 0 }, "300");
   });
 
+  /*  A recursive function that fades the logo to 10% visibility and back to 100% */
   function pulse() {
     $("#logo")
       .fadeTo(2000, 0.1)
       .fadeTo(2000, 1, pulse);
   }
   pulse();
+
+  // Changing product background color
+  $('[data-type="mineralwater"]').css("background", "green");
+
+  //Make first word of person cards bold
+  $(".person-card p").each(function() {
+    var html = $(this).html();
+    var word = html.substr(0, html.indexOf(" "));
+    var rest = html.substr(html.indexOf(" "));
+    $(this)
+      .html(rest)
+      .prepend($("<strong/>").html(word));
+  });
+
+  //Move Michael Lewiston to the top of personal cards
+  $(".person-card:contains(Lewiston)").prependTo(".people-cards");
+
+  //Change 'People' to 'Personnel' in navbar and topic
+  $(".navbar li a:contains(people)").text("Personnel");
+  $("#people .headline").text("Personnel");
 });
